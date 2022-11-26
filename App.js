cc84d5d54e68f148a624809f76b7f84c9c7956f6
Aleksi Nokela <n0noal00@students.oamk.fi>
2022-11-26 19:11:14 +0200

    push to repository

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useCallback, useEffect } from 'react'
+import { GiftedChat } from 'react-native-gifted-chat';
+import uuid from 'react-uuid';
+import * as Speech from 'expo-speech';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [messages, setMessages] = useState([]);
+  const OPENING_MESSAGE = 'Hello developer';
+  const replies = [
+    'How are you?',
+    'Nice to chat with you',
+    'That is impressive',
+    'Excuse me',];
+useEffect(() => {
+  Speech.speak(OPENING_MESSAGE);
+  setMessages([
+    {
+      _id: 1,
+      text: OPENING_MESSAGE,
+      createdAt: new Date(),
+      user: {
+        _id: 2,
+        name: '?',
+      },
+    },
+  ])
+}, [])
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const onSend = useCallback((messages = []) => {
+  setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+  let messageText = replies[Math.floor(Math.random() * replies.length)]  
+  let newMessage = 
+  [
+      {
+          _id: uuid(),
+          text: messageText,
+          createdAt: new Date(),
+          user: {
+              _id: 2,
+              name: '?'
+          },
+      },
+  ]
+  setMessages(previousMessages => GiftedChat.append(previousMessages, newMessage))
+  Speech.speak(messageText)
+}, [])
+
+return (
+  <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+    />
+)
+}
